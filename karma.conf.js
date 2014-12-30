1bad12288ff1049f25f1f988640dc8e2b73b1cb9
Tomotaka Sakuma <ktmtmks@gmail.com>
2014-12-30 17:44:32 +0900

    karma options for travis

--- karma.conf.js
+++ karma.conf.js
@@ -81,6 +81,14 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
+    singleRun: true,
+
+
+    customLaunchers: {
+      ChromeTravis: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
   });
 };
